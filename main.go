3394bbe1bd2d8c97924b13bbc96898cf4dd23814
ing.leonardovega@gmail.com <leonardovega>
2020-06-21 05:31:04 -0500

    modulos de order terminado

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/GolangPruebaRestApi/customer"
 	"github.com/GolangPruebaRestApi/database"
 	"github.com/GolangPruebaRestApi/employee"
+	"github.com/GolangPruebaRestApi/order"
 	"github.com/GolangPruebaRestApi/product"
 	"github.com/go-chi/chi"
 	_ "github.com/go-sql-driver/mysql"
@@ -19,22 +20,26 @@ func main() {
 		productRepository  = product.NewRepository(databaseConnection)
 		employeeRepository = employee.NewRepository(databaseConnection)
 		customerRepository = customer.NewRepository(databaseConnection)
+		orderRepository    = order.NewRepository(databaseConnection)
 	)
 
 	var (
 		productService  product.Service
 		employeeService employee.Service
 		customerService customer.Service
+		orderService    order.Service
 	)
 
 	productService = product.NewService(productRepository)
 	employeeService = employee.NerService(employeeRepository)
 	customerService = customer.NewService(customerRepository)
+	orderService = order.NewService(orderRepository)
 
 	r := chi.NewRouter()
 	r.Mount("/products", product.MakeHttpHandler(productService))
 	r.Mount("/employees", employee.MakeHttpHandler(employeeService))
 	r.Mount("/customers", customer.MakeHTTPHandler(customerService))
+	r.Mount("/orders", order.MakeHTTPHandler(orderService))
 	http.ListenAndServe(":3000", r)
 
 }
